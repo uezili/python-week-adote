44be03e2e01ac81542aeb8b0f517dcab15179881
uezili <weslmaciel@gmail.com>
2023-01-16 18:35:48 -0300

    New router listar_pets for app adotar.

--- adote/urls.py
+++ adote/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('divulgar/', include('divulgar.urls')),
+    path('adotar/', include('adotar.url'))
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
